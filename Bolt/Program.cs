008999cdbe42c55811be6abd848236a0d28a680e
LCervus <102804734+KendodiK@users.noreply.github.com>
2024-03-01 12:26:32 +0100

    id-t ad hozzá

--- Bolt/Program.cs
+++ Bolt/Program.cs
@@ -11,18 +11,25 @@ namespace Bolt
     {
         class Eszkozok
         {
+            private int id;
             private string nev;
             private int ar;
             private int mennyiseg;
             private string parameterek;
+            private bool elavulo;
 
-            public Eszkozok(string nev, int ar, int mennyiseg, string parameterek)
+            public Eszkozok(string nev, int ar, int mennyiseg, string parameterek, bool elavulo)
             {
                 this.nev = nev;
                 this.ar = ar;
                 this.mennyiseg = mennyiseg;
                 this.parameterek = parameterek;
+                this.elavulo = elavulo;
+
             }
+
+            public int setId(int id) => this.id = id; public int getId() => this.id;
+
         }
 
 
@@ -36,9 +43,22 @@ namespace Bolt
             StreamReader sr = new StreamReader("adatok.txt");
             while (sr.EndOfStream)
             {
+                bool avul = false;
                 string sor = sr.ReadLine();
                 string[] sorok = sor.Split(';');
-                Eszkozok eszkoz = new Eszkozok(sorok[0], Convert.ToInt32(sorok[1]), Convert.ToInt32(sorok[2]), sorok[3]);
+                if (sorok[4] == "igen")
+                {
+                    avul = true;
+                }
+                Eszkozok eszkoz = new Eszkozok(sorok[0], Convert.ToInt32(sorok[1]), Convert.ToInt32(sorok[2]), sorok[3], avul);
+                if (Adatok.Count > 0)
+                {
+                    int utoloId = Adatok[Adatok.Count - 1].getId();
+                    eszkoz.setId(utoloId + 1);
+                } else
+                {
+                    eszkoz.setId(0);
+                }
                 Adatok.Add(eszkoz);
             }
             sr.Close();
